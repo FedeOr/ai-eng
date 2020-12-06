007b76ee5801b3446f0e3723c848dfdea8e246b7
federica-origgi <federica.origgi@allianz.it>
2020-12-06 14:34:26 +0100

    ristrutturato codice

--- StreamlitCampus/app.py
+++ StreamlitCampus/app.py
@@ -7,9 +7,10 @@ import requests
 import os
 import datetime
 import pyodbc
-from azure.storage.blob import BlobServiceClient, BlobClient
 
+from azure.storage.blob import BlobServiceClient, BlobClient
 from PIL import Image
+from src.model import predict_model
 
 def connect_DB(tableName):
     
@@ -26,35 +27,35 @@ def connect_DB(tableName):
     
     return df_database
     
-def dataset_transform(df_log):
-    # Valore di TelegramType da mantenere
-    ttype = 'write      '
-    # Creazione dizionario
-    d = {'W':1, '%':2, 'ppm':3, 'C°':4, 'Wh':5, 'bool':6}
-    # Rimozione righe con TelegramType = 'response'
-    df_log = df_log.loc[df_log.TelegramType==ttype]
-    # Trasformo i dati boolean in modo da poter essere interpretati dal modello
-    df_log.loc[df_log.Value=='False',["Value"]] = 0
-    df_log.loc[df_log.Value=='True',["Value"]] = 1
-    # Creazione della colonna a partire dal mapping del dizionario
-    df_log['IdType'] = df_log['ValueType'].map(d)
-    # Conversione colonna 'Data'
-    df_log['Data'] = pd.to_datetime(df_log['Data'])
-    # Conversione colonna 'Value'
-    df_log["Value"] = pd.to_numeric(np.char.replace(df_log["Value"].to_numpy().astype(str),',','.'))
+# def dataset_transform(df_log):
+#     # Valore di TelegramType da mantenere
+#     ttype = 'write      '
+#     # Creazione dizionario
+#     d = {'W':1, '%':2, 'ppm':3, 'C°':4, 'Wh':5, 'bool':6}
+#     # Rimozione righe con TelegramType = 'response'
+#     df_log = df_log.loc[df_log.TelegramType==ttype]
+#     # Trasformo i dati boolean in modo da poter essere interpretati dal modello
+#     df_log.loc[df_log.Value=='False',["Value"]] = 0
+#     df_log.loc[df_log.Value=='True',["Value"]] = 1
+#     # Creazione della colonna a partire dal mapping del dizionario
+#     df_log['IdType'] = df_log['ValueType'].map(d)
+#     # Conversione colonna 'Data'
+#     df_log['Data'] = pd.to_datetime(df_log['Data'])
+#     # Conversione colonna 'Value'
+#     df_log["Value"] = pd.to_numeric(np.char.replace(df_log["Value"].to_numpy().astype(str),',','.'))
     
-    return df_log
+#     return df_log
 
-def preprocessing(df_log):
+# def preprocessing(df_log):
     
-    # Estraggo l'ora dalla data della rilevazione
-    df_log['Hour'] = pd.DatetimeIndex(df_log['Data']).hour
-    # Rimozione colonne non necessarie per il training del modello
-    column_to_drop = ['Data','IndividualAddress','GroupAddress','TelegramType','ValueType','Description']
-    df_log.drop(column_to_drop, axis=1, inplace=True)
-    # df_log.drop(column_to_drop)
+#     # Estraggo l'ora dalla data della rilevazione
+#     df_log['Hour'] = pd.DatetimeIndex(df_log['Data']).hour
+#     # Rimozione colonne non necessarie per il training del modello
+#     column_to_drop = ['Data','IndividualAddress','GroupAddress','TelegramType','ValueType','Description']
+#     df_log.drop(column_to_drop, axis=1, inplace=True)
+#     # df_log.drop(column_to_drop)
     
-    return df_log
+#     return df_log
 
 def main_menu():
     try:
@@ -147,9 +148,9 @@ def side_menu():
                     df = connect_DB(table_name)
                     #df = pd.read_csv("data\log2.csv", sep=';', encoding='cp1252')
                     
-                    df_transform = dataset_transform(df)
+                    df_transform = predict_model.dataset_transform(df)
                     
-                    df_preprocess = preprocessing(df_transform)
+                    df_preprocess = predict_model.preprocessing(df_transform)
                                 
                     prediction = score_model(df_preprocess)
                     df['Prediction'] = prediction
